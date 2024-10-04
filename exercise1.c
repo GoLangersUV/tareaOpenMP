06b25e3885463b261ef0cb9200ee53e63a288009
Juan Molina <juan.sebastian.molina@correounivalle.edu.co>
2024-10-04 09:59:51 -0500

    narvaez solution
    Co-authored-by: Ulvenforst <juan.narvaez.tascon@correounivalle.edu.co>
    Co-authored-by: cristiandpt <cristian.pacheco@correounivalle.edu.co>
    Co-authored-by: puyojulian <julian.puyo@correounivalle.edu.co>

--- exercise1.c
+++ exercise1.c
@@ -4,13 +4,12 @@
 int main() {
     #pragma omp parallel num_threads(10)
     {
-        if(omp_get_thread_num()%2){
-            printf("Hello even from thread %d\n",omp_get_thread_num());
+        int thread_num = omp_get_thread_num();
+        if (thread_num % 2 == 0) {
+            printf("Hola desde el hilo par %d\\n", thread_num);
+        } else {
+            printf("Hola desde el hilo impar %d\\n", thread_num);
         }
-        else{
-            printf("Hello odd from thread %d\n",omp_get_thread_num());
-        }
-        
     }
     return 0;
 }
